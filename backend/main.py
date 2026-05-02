93693f75a2c624dec3817f231e8ade41656233cd
mngajbhiye <mohitgajbhiye9@gmail.com>
2026-05-02 18:06:10 +0530

    fast api implementation - api for background removal

---
+++ backend/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
+from rembg import remove
+import io
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost:5173",  # your React app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # or ["*"] for testing
+    allow_credentials=True,
+    allow_methods=["*"],          # GET, POST, etc.
+    allow_headers=["*"],          # all headers
+)
+
+
+@app.post("/photolab/remove-bg/")
+async def remove_bg(file: UploadFile = File(...)):
+
+    # Read image bytes
+    input_bytes = await file.read()
+
+    # Remove background
+    output_bytes = remove(input_bytes)
+
+    # Return processed image
+    return StreamingResponse(
+        io.BytesIO(output_bytes),
+        media_type="image/png"   # PNG because background is transparent
+    )
+
